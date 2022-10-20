10ac715706a3a32b968ec227b9b4c494136036da
Shuying Liu <shuyingliu24@gmail.com>
2022-10-20 23:25:15 +0100

    added dloop

--- proj2.R
+++ proj2.R
@@ -6,42 +6,43 @@
 
 #XXXXXXXXXXXXXXXXXXXXXXXXXXX Contribution XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-# Biwei Zhu completed the Question 1
-# Guanhao Su completed the Question 2
-# Biwei Zhu and Guanhao Su completed the question 3 and 4 together
-# Shuying Liu completed the Question 5,6 and modified the whole code. 
+# Biwei Zhu completed the function Pone
+# Guanhao Su completed the function Pall and the visualization
+# Biwei Zhu and Guanhao Su completed together the example code to assess success probabilities under each strategy
+# Shuying Liu completed the function dloop and the assessment of probability, and modified the whole code. 
 
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXX Variables XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXX Simulation XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 
-# This project aims to calculate the surviving probability of 2n prisoners
+# This project aims to estimate the success probability of the prisoner problem by simulations
 # under three strategies:
 
-# 1. The prisoner starts at the box with their number on it, opens it and 
-# reads the number on the card: k, say. If k is not their prisoner number, 
-# they go to box number k, open it and repeat the process until they have either
-# found the card with their number on it, or opened n boxes without finding it.
+# 1. The prisoner first opens the box with their number on it, 
+# reads the number in the box then opens the box with that number,
+# repeat until finding their own number in the box or reach the maximum of n open boxes.
+
 
 # 2. As strategy 1, but starting from a randomly selected box.
 
-# 3. They open n boxes at random, checking each card for their number.
-# Note: The card in 2n boxes are in random order.
+# 3. They open n boxes at random to look for their number.
+# Note: The cards in 2n boxes are in random order.
 
-
-# Estimate the probability of a single prisoner succeeding in finding his number
-# under three strategies.
-# nreps is the number of replicate simulations.
-# success_one is used to count the overall success times.
 
 sim <- function(n,k,strategy,nprisoner=1,nreps=10000){
+  # sim estimates the probability of prisoner succeeding in finding his number in 2*n boxes
+  # takes argument: n; k - the list of prisoners' numbers; strategy - 1,2,3;
+  #                 nprisoner - number of prisoners; nreps - number of simulations
+  # returns the estimate probability
   
+  # the total numer of success in nreps simulations
   success <- 0
   
+  # Run nreps simulations
   for (irep in 1:nreps) {
     cards <- sample(2*n)
-    # check_cards <- array(0,n)
     flag <- array(FALSE,nprisoner)
     
+    # Check if each prisoner succeeds
     for (prisoner in 1:nprisoner) {
       if (strategy == 3) {
         open_boxes <- sample(2*n,n)
@@ -49,12 +50,10 @@ sim <- function(n,k,strategy,nprisoner=1,nreps=10000){
         if (k[prisoner] %in% check_cards) flag[prisoner] <- TRUE
       }
       else {
-        # if (strategy == 1) check_cards[1] <- cards[k[prisoner]]
-        # else check_cards[1] <- cards[sample(2*n,1)]
+        # Different initially selected box number for strategy 2 & 3
         if (strategy == 1) selected_box <- k[prisoner]
         else selected_box <- sample(2*n,size=1)
         
-        # for (box in 2:n) check_cards[box] <- cards[check_cards[box-1]]
         for (box in 1:n){
           if (cards[selected_box] == k[prisoner]){
             flag[prisoner] <- TRUE
@@ -69,27 +68,19 @@ sim <- function(n,k,strategy,nprisoner=1,nreps=10000){
   success/nreps
 }
 
-# time <- function(){
-#   start.time <- Sys.time()
-#   sim(50,1:100,1,100)
-#   end.time <- Sys.time()
-#   time.taken <- end.time - start.time
-#   time.taken
-# }
-# time()
 
 Pone <- function(n,k,strategy,nreps=10000){
+  # Pone estimates the probability of one prisoner succeeding in finding his number in 2*n boxes
+  # Takes argument: n; k - the prisoner's number; strategy - 1,2,3;
+  #                 nreps - number of simulations
+  # Returns the estimate probability
   sim(n,k,strategy)
 }
 
-
-
-# Estimate the probability of all prisoners finding their number, so that all 
-# are released, under three strategies.
-# success_all is used to note which prisoner has succeed.
-# final_success_all is used to count if 2n prisoners are released in jth experiment.
-
 Pall <- function(n,strategy,nreps=10000){
+  # Pone estimates the probability of all 2*n prisoners succeeding in finding their number in 2*n boxes
+  # Takes argument: n; strategy - 1,2,3; nreps - number of simulations
+  # Returns the estimate probability
   k <- 1:(2*n)
   sim(n,k,strategy,nprisoner=2*n)
 }
@@ -112,43 +103,54 @@ for (strategy in 1:3) cat(sprintf('strategy %d: %f\n',strategy,Pall(50,strategy,
 
 
 #XXXXXXXXXXXXXXXXXXXXXXXXXXXX comments XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-# For one prisoner
-# When a prisoner is simulated enough times, the prisoner can be successfully released is independent of the number of boxes.
-# Surprisingly, the only difference between the strategy 2 and strategy 1 is the initial choice of boxes,
-# but strategy 2 (prob = 0.4) will have a lower probability of success than strategies 1 & 3 (prob = 0.5).
-# Speculation: If strategy 1 is chosen, then the number k can be guaranteed to be in the loop.
-# So the prisoner's success depends only on the depth of the loop (Only if depth <= n can be succeed)
-# However, in strategy 2, the prisoner cannot guarantee that his number is in the loop
+# If we run enough simulations, we can estimate the probability by the relative frequency.
+# The only difference between strategy 2 and strategy 1 is the initial choice of boxes,
+# but strategy 2 (prob = 0.4) will have a lower probability of success for one prisoner than strategies 1 & 3 (prob = 0.5).
+# The difference of success rate becomes greater as we run simulations on 2*n prisoners,
+# strategy 1 still has a surprisingly stable success rate (Prob = 0.3),
+# while the success rates of strategy 2 & 3 drop to 0.
 
-# For 2*n prisoners
-# As the number of prisoners increases,
-# strategy 1 (Prob = 0.318) is more stable compared to strategies 2,3 (which converge to 0).
+# Speculation: Strategy 1 works surprisingly well because there is a decent chance that
+# the length of a loop in a 2*n permutation is under n,
+# thus if the prisoner first opens the box with his own number, then he is on the unique loop containing his number,
+# so he is likely to find his number in the sequence of opening n boxes.
+# Strategy 2 fails because he initially opens a random box, so loop wouldn't help.
+
 #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 dloop <- function(n,nreps=10000){
+  # dloop estimates the probability of each loop length from 1 to 2*n ocurring in a random 2*n permutation
+  # Take argument: n; nreps - number of simulations
+  # Returns a matrix of diminsion (2,2*n), 
+  #   the first row being the probability distribution of each loop length, 
+  #   the second being the probability distribution of the longest loop.
   
   loops <- array(0,2*n)
   longest_loops <- array(0,2*n)
   
+  # run nreps simulations
   for (irep in 1:nreps){
-    u <- sample(2*n)
+    # ramdom shuffle of cards
+    shuffle <- sample(2*n)
+    # elements of loop_check are whether each loop length ocurrs in shuffle
     loop_check <- array(0,2*n)
+    # elements of unchecked_list are whether indices of shuffle have been searched for loop
     unchecked_list <- array(TRUE,2*n)
     
-    # search all loops
-    while (length(u[unchecked_list])){
-      start <- u[unchecked_list][1]
+    # search all loops until all the elements in shuffle have been searched
+    while (length(shuffle[unchecked_list])){
+      # pick an unchecked element in shuffle to start
+      start <- shuffle[unchecked_list][1]
       k <- start
       unchecked_list[k] <- FALSE
       loop_len <- 1
       # find the loop that begins with 'start'
-      while (u[k] != start){
-        k <- u[k]
+      while (shuffle[k] != start){
+        k <- shuffle[k]
         unchecked_list[k] <- FALSE
         loop_len <- loop_len+1
       }
       loop_check[loop_len] <- 1
-      
     }
     
     loops <- loops+loop_check
@@ -157,12 +159,17 @@ dloop <- function(n,nreps=10000){
   }
   rbind(loops/nreps,longest_loops/nreps)
 }
+
+# Assess the probability of loop
 y <- dloop(50)
-(y[1,])#print the probability of each loop length from 1 to 2n occuring at least once.
+# The probability of each loop length from 1 to 2n occuring at least once.
+cat('The probability of each loop length is\n',(y[1,]),'\n')
+# the probability that there is no loop longer than 50 is roughly 0.31
+cat('Probability of longest loop length being 50 is',sum(y[2,1:50]))
 
 
-#Visualising the probability sensibly.
-n = 50
+#Visualising the probability
+n <- 50
 color1 <- c(rgb(250,220,120,150,max = 255), rgb(160,150,180,150,max = 255))
 color2 <- c(rep(rgb(250,220,120,150,max = 255),n), rep(rgb(160,150,180,150, max = 255), n))
 par(mfrow = c(1,3))#three subplot
@@ -171,10 +178,10 @@ barplot(y[1,],col = rgb(255,100,100,150,max = 255), space = 0,xaxs="i"
         , xlab = "Length of the loop", ylab = "Probability")
 axis(1,c(0,50,100))
 
-barplot(c(sum(y[2,1:50]), sum(y[2,51:100])), space = 0, col = color1, xaxs="i"
-        , xlab = "Length of the loop \nlonger than 50 or not", ylab = "Cumulative Probability")
-axis(1,c(0,1,2),c(0,50,100))
-
 barplot(c((y[2,1:50]),(y[2,51:100])), space = 0, col = color2, xaxs="i"
         , xlab = "Length of the longest loop", ylab = "Probability")
 axis(1,c(0,50,100))
+
+barplot(c(sum(y[2,1:50]), sum(y[2,51:100])), space = 0, col = color1, xaxs="i"
+        , xlab = "Length of the loop \nlonger than 50 or not", ylab = "Cumulative Probability")
+axis(1,c(0,1,2),c(0,50,100))
